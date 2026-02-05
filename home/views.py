27cb6c59567c111519860b4b29a5bf12da3494df
rishanvc <rishanvcmuhammed@gmail.com>
2026-02-06 01:13:33 +0530

    second commit

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from home.models import Person
+from home.serializer import PersonSerializer
 
 
 
@@ -20,4 +22,44 @@ def index(request):
      
     elif request.method=='DELETE':
         return Response('This is DELETE method')
+    
+
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def person(request):
+    if request.method=='GET':
+        objPerson=Person.objects.all()
+        serializer=PersonSerializer(objPerson,many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        data=request.data
+        serializer=PersonSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method=='PUT':
+        data=request.data
+        obj=Person.objects.get(id=data['id'])
+        serializer=PersonSerializer(obj,data=data,partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method=='PATCH':
+        data=request.data
+        obj=Person.objects.get(id=data['id'])
+        serializer=PersonSerializer(obj,data=data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    else:
+        data=request.data
+        obj=Person.objects.get(id=data['id'])
+        obj.delete()
+        return Response({'messege':'person delete'})
+
+
+
      
